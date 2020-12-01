07a1a1cf8ed5a464130a33971cbe6fdec4abdca6
PureFunctor <purefunctor@gmail.com>
2020-12-01 23:33:21 +0800

    Add Part 2 for Haskell

--- DayOne.hs
+++ DayOne.hs
@@ -5,8 +5,18 @@ import Data.Set as S
 parseInputs :: IO (S.Set Int)
 parseInputs = S.fromList . (read <$>) . lines <$> readFile "./day_1.txt"
 
-solution :: S.Set Int -> Int
-solution xs = S.foldl' (*) 1 . S.filter (`S.member` xs) . S.map (2020 -) $ xs
+solution_1 :: S.Set Int -> Int
+solution_1 xs = S.foldl' (*) 1 . S.filter (`S.member` xs) . S.map (2020 -) $ xs
+
+solution_2 :: S.Set Int -> Int
+solution_2 xs = head [ (2020 - y0 - y1) * y0 * y1
+                     | y0 <- ys, y1 <- ys, S.member (2020 - y0 - y1) xs
+                     ]
+  where
+    ys = S.toList xs
 
 main :: IO ()
-main = parseInputs >>= (print . solution)
+main = do
+  xs <- parseInputs
+  print . solution_1 $ xs
+  print . solution_2 $ xs
