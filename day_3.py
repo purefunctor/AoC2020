5e7db914da6d77494a73357f37c3ca61ab33a254
PureFunctor <purefunctor@gmail.com>
2020-12-03 14:50:49 +0800

    Add Day Three Part Two

--- day_3.py
+++ day_3.py
@@ -7,24 +7,49 @@ DELTA_Y = 1
 
 def parse_inputs():
     with open("./day_3.txt", "r") as f:
-        lines = f.readlines()
+        lines = list(map(str.strip, f.readlines()))
 
     max_y = len(lines)
-    max_x = len(lines[0]) - 1
+    max_x = len(lines[0])
 
     return max_x, max_y, lines
 
 
-def solution():
+def solution_1():
     max_x, max_y, lines = parse_inputs()
 
     coords = zip(
-        (x % max_x for x in count(3, 3)),
-        (range(1, max_y))
+        (x % max_x for x in count(DELTA_X, DELTA_X)),
+        (range(DELTA_Y, max_y, DELTA_Y))
     )
 
     print(sum(lines[y][x] == "#" for x, y in coords))
 
 
+def solution_2():
+    max_x, max_y, lines = parse_inputs()
+
+    def create_coords(dx, dy):
+        return zip(
+            (x % max_x for x in count(dx, dx)),
+            (range(dy, max_y, dy)),
+        )
+
+    deltas = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2)
+    ]
+
+    prod = 1
+    for dx, dy in deltas:
+        prod *= sum(lines[y][x] == "#" for x, y in create_coords(dx, dy))
+
+    print(prod)
+
+
 if __name__ == "__main__":
-    solution()
+    solution_1()
+    solution_2()
