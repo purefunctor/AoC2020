4c784b5d1d99f0dc35f2341cf529e8511051c824
PureFunctor <purefunctor@gmail.com>
2020-12-05 17:43:08 +0800

    Refactor Day Five to add types

--- DayFive.hs
+++ DayFive.hs
@@ -3,36 +3,41 @@ module Main where
 import Data.List (sort)
 
 type Bounds = (Int, Int)
+type Partition = Char
+type Partitions = [Partition]
+type SeatNumber = Int
 
-parseInput :: IO [(String, String)]
+parseInput :: IO [(Partitions, Partitions)]
 parseInput = (splitAt 7 <$>) . lines <$> readFile "./day_5.txt"
 
-binaryPartition_ :: Char -> Bounds -> Bounds
+binaryPartition_ :: Partition -> Bounds -> Bounds
 binaryPartition_ c (l, u)
   | c == 'F' || c == 'L' = (l, m)
   | c == 'B' || c == 'R' = (m + 1, u)
   where
     m = u `div` 2 + l `div` 2
 
-binaryPartition :: String -> Bounds -> Bounds
+binaryPartition :: Partitions -> Bounds -> Bounds
 binaryPartition = foldl1 (flip (.)) . map binaryPartition_
 
-getSeats :: [(String, String)] -> [Int]
-getSeats lines = [getRow rs + getCol cs | (rs, cs) <- lines]
+partitionSeats :: [(Partitions, Partitions)] -> [Int]
+partitionSeats partitions = [getRow row_part + getCol col_part
+                      | (row_part, col_part) <- partitions
+                      ]
   where
-    getRow rs = fst (binaryPartition rs (0, 127)) * 8
-    getCol cs = fst (binaryPartition cs (0, 7))
+    getRow row_part = fst (binaryPartition row_part (0, 127)) * 8
+    getCol col_part = fst (binaryPartition col_part (0, 7))
 
-solution_1 :: [(String, String)] -> Int
-solution_1 = maximum . getSeats
+solution_1 :: [(Partitions, Partitions)] -> SeatNumber
+solution_1 = maximum . partitionSeats
 
-solution_2 :: [(String, String)] -> Int
-solution_2 = gapped . sort . getSeats
+solution_2 :: [(Partitions, Partitions)] -> SeatNumber
+solution_2 = gapped . sort . partitionSeats
   where
     gapped (x:y:rest) = if x - y == -2 then (y - x - 1) + x else gapped rest
 
 main :: IO ()
 main = do
-  lines <- parseInput
-  print . solution_1 $ lines
-  print . solution_2 $ lines
+  partitions <- parseInput
+  print . solution_1 $ partitions
+  print . solution_2 $ partitions
