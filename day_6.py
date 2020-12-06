f8e711aa2fbb28ebfdf97779af09c69064f6c7f4
PureFunctor <purefunctor@gmail.com>
2020-12-06 13:19:12 +0800

    Add Day Six Part Two

--- day_6.py
+++ day_6.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def parse_inputs():
     with open("./day_6.txt", "r") as f:
         return [line.strip().split("\n") for line in f.read().split("\n\n")]
@@ -10,5 +13,14 @@ def solution_1():
     return n
 
 
+def solution_2():
+    n = 0
+    for group in parse_inputs():
+        new_group = list(reduce(lambda x, y: x.intersection(y), map(set, group)))
+        n += len(new_group)
+    return n
+
+
 if __name__ == "__main__":
     print(solution_1())
+    print(solution_2())
