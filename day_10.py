61d7a3834da352b61246ada13f4f967a03b1f081
PureFunctor <purefunctor@gmail.com>
2020-12-11 16:32:46 +0800

    Add Day Ten Part Two

--- day_10.py
+++ day_10.py
@@ -1,3 +1,6 @@
+from functools import lru_cache
+
+
 def parse_inputs():
     with open("./day_10.txt") as f:
         return [int(line) for line in f.readlines()]
@@ -21,5 +24,41 @@ def solution_1():
     return deltas[0] * deltas[2]
 
 
+def solution_2():
+    joltages = set(parse_inputs())
+    joltages.add(0)
+    joltages.add(max(joltages) + 3)
+
+    joltages_subpaths = {
+        joltage: [
+            expected
+            for expected in range(joltage + 1, joltage + 4)
+            if expected in joltages
+        ]
+        for joltage in joltages
+    }
+
+    @lru_cache(maxsize=None)
+    def check_branching(current):
+        subpaths = joltages_subpaths[current]
+        possible = len(subpaths)
+        if possible == 0:
+            return 0
+        elif possible == 1:
+            return check_branching(subpaths[0])
+        else:
+            paths = 0
+            for subpath in subpaths:
+                branches = check_branching(subpath)
+                if branches == 0:
+                    paths += 1
+                else:
+                    paths += branches
+            return paths
+
+    return check_branching(0)
+
+
 if __name__ == "__main__":
     print(solution_1())
+    print(solution_2())
