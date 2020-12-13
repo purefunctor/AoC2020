8efde7f1ac7036ba8c0b4a41040d816f9f5e9fbe
PureFunctor <purefunctor@gmail.com>
2020-12-13 17:51:47 +0800

    Add Day Eleven Part Two
    Scuffed code is scuffed

--- day_11.py
+++ day_11.py
@@ -16,6 +16,25 @@ def get_adjacent(x, y):
     ]
 
 
+def make_beam_gun(size_x, size_y):
+    @lru_cache(maxsize=None)
+    def beam_gun(x, y):
+        beams = []
+        for (xd, yd) in get_adjacent(0, 0):
+            beam_group = []
+            multiplier = 1
+            while True:
+                xn = xd * multiplier + x
+                yn = yd * multiplier + y
+                if 0 > xn or xn >= size_x or 0 > yn or yn >= size_y:
+                    break
+                beam_group.append((xn, yn))
+                multiplier += 1
+            beams.append(beam_group)
+        return beams
+    return beam_gun
+
+
 def board_mutate(mapping):
     _mapping = {}
     for (x, y), v in mapping.items():
@@ -60,5 +79,70 @@ def solution_1():
         mapping_1, mapping_2 = mapping_2, board_mutate(mapping_2)
 
 
+def board_mutate_with_beam_gun(mapping, beam_gun):
+    _mapping = {}
+    for (x, y), v in mapping.items():
+        if v == "L":
+            for beam_group in beam_gun(x, y):
+                do = ""
+                for xn, yn in beam_group:
+                    seeing = mapping[(xn, yn)]
+                    if seeing == ".":
+                        continue
+                    elif seeing == "L":
+                        do = "continue"
+                        break
+                    else:
+                        do = "break"
+                        break
+                if do == "continue":
+                    continue
+                elif do == "break":
+                    _mapping[(x, y)] = "L"
+                    break
+            else:
+                _mapping[(x, y)] = "#"
+        elif v == "#":
+            n = 0
+            for beam_group in beam_gun(x, y):
+                next_group = False
+                for xn, yn in beam_group:
+                    seeing = mapping[(xn, yn)]
+                    if seeing == ".":
+                        continue
+                    elif seeing == "L":
+                        next_group = True
+                        break
+                    else:
+                        n += 1
+                        break
+                if next_group:
+                    continue
+            if n >= 5:
+                _mapping[(x, y)] = "L"
+            else:
+                _mapping[(x, y)] = "#"
+        elif v == ".":
+            _mapping[(x, y)] = v
+    return _mapping
+
+
+def solution_2():
+    xs = parse_inputs()
+    mapping_1 = {
+        (x, y): v
+        for x, ys in enumerate(xs)
+        for y, v in enumerate(ys)
+    }
+    beam_gun = make_beam_gun(len(xs), len(xs[0]))
+    mapping_2 = board_mutate_with_beam_gun(mapping_1, beam_gun)
+
+    while True:
+        if mapping_1 == mapping_2:
+            return sum(v == "#" for v in mapping_2.values())
+        mapping_1, mapping_2 = mapping_2, board_mutate_with_beam_gun(mapping_2, beam_gun)
+
+
 if __name__ == "__main__":
     print(solution_1())
+    print(solution_2())
