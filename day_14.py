9da593aa85ba3a3f1a6d984ef65b512152426300
PureFunctor <purefunctor@gmail.com>
2020-12-14 14:16:27 +0800

    Add Day Fourteen Part Two

--- day_14.py
+++ day_14.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 
@@ -29,5 +30,37 @@ def solution_1():
     return sum(addresses.values())
 
 
+def calculate_floating(floating):
+    l = floating.count("X")
+    for f in itertools.product("01", repeat=l):
+        yield floating.replace("X", "{}").format(*f)
+
+
+def solution_2():
+    mask_re = re.compile("mask = ([01X]+)")
+    inst_re = re.compile(r"mem\[(\d+)\] = (\d+)")
+
+    addresses = {}
+    for mask_, *instructions_ in parse_inputs():
+        mask = mask_re.match(mask_)[1]
+        instructions = [inst_re.match(instruction).groups() for instruction in instructions_]
+
+        for address, bits in instructions:
+            unpadded = list(bin(int(address)))[2:]
+            bit_str= ["0" for _ in range(len(mask) - len(unpadded))] + unpadded
+            floating = "".join(
+                b if m == "0" else
+                m if m == "1" else
+                "X"
+                for m, b in zip(mask, bit_str)
+            )
+
+            for filled in calculate_floating(floating):
+                addresses[int(filled, 2)] = int(bits)
+
+    return sum(addresses.values())
+
+
 if __name__ == "__main__":
     print(solution_1())
+    print(solution_2())
