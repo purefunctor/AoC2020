ae53a50cf195e9e764998774eeda08f926d41a0c
PureFunctor <purefunctor@gmail.com>
2020-12-15 15:07:51 +0800

    Add Day Fifteen Part Two
    Terrible RAM hog, run with PyPy

--- day_15.py
+++ day_15.py
@@ -1,30 +1,33 @@
-from collections import deque
+from collections import defaultdict, deque
 
 
-xs = "11,0,1,10,5,19"
+xs = [int(x) for x in "11,0,1,10,5,19".split(",")]
+
+
+def solution(xs, lim):
+    latest = xs[-1]
+    spoken = defaultdict(deque, {x: deque([turn]) for turn, x in enumerate(xs, start=1)})
+    for turn in range(len(xs) + 1, lim + 1):
+        if len(spoken[latest]) == 1:
+            latest = 0
+            spoken[0].append(turn)
+        else:
+            first = spoken[latest].pop()
+            second = spoken[latest].pop()
+            spoken[latest].append(first)
+            latest = first - second
+            spoken[latest].append(turn)
+    return latest
 
 
 def solution_1():
-    numbers = deque(int(x) for x in reversed(xs.split(",")))
-    spoken = {number: 1 for number in numbers}
-    for _ in range(2020 - len(numbers)):
-        if spoken.get(numbers[0], None) == 1:
-            numbers.appendleft(0)
-            spoken[0] += 1
-        else:
-            finder = (
-                len(numbers) - index
-                for index, number in enumerate(numbers)
-                if number == numbers[0]
-            )
-            found = next(finder) - next(finder)
-            numbers.appendleft(found)
-            if found not in spoken:
-                spoken[found] = 0
-            spoken[found] += 1
+    return solution(xs, 2020)
 
-    return numbers[0]
+
+def solution_2():
+    return solution(xs, 30000000)
 
 
 if __name__ == "__main__":
     print(solution_1())
+    print(solution_2())
